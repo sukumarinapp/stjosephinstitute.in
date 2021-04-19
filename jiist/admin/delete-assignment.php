ba8cd9f7b2db3262abe310edac976686e9b3526d
Galaxy Kannan <universekannan@gmail.com>
2021-04-19 11:56:48 +0530

    add assignment

--- jiist/admin/delete-assignment.php
+++ jiist/admin/delete-assignment.php
@@ -7,6 +7,13 @@ include "config.php";
 $id=$_GET['id'];
 if (($_SESSION['user_type'] != "Superadmin") && ($_SESSION['user_type'] != "Admin")) header("location: index.php");
  else{
+ 	
+ 	$sql2 = "select * from jiier_assignment where id=$id";
+	$result2 = mysqli_query($conn, $sql2);
+	$row2 = mysqli_fetch_assoc($result2);
+	$assignment = $row2['question'];
+	unlink("assignment/$assignment");
+
     $sql = "delete from jiier_assignment where id=$id";
     mysqli_query($conn, $sql);
     header("location: assignment.php");
